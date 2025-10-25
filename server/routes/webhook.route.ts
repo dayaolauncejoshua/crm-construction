b1cde1bd38807dd11aeebe07e3409a001d73a2d3
MarkNeil03-hub <MarkNeilTanapWorkEmail@gmail.com>
2025-10-25 21:27:53 +0800

    backups and cleanups

--- server/routes/webhook.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-import { webHookController } from "server/controllers/webhook.controller";
-const router = Router();
-
-router.post("/", webHookController);
-
-export default router;
