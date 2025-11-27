358f015a2c5e092d9aa85568e86c2ca2952ad695
dayaolauncejoshua <dayaolauncejoshua@gmail.com>
2025-11-27 10:40:46 +0800

    simplify vite build paths for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -9,20 +9,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
+  root: "./client",
   build: {
-    outDir: path.resolve(__dirname, "dist", "public"),
+    outDir: "../dist",
     emptyOutDir: true,
-  },
-  server: {
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
-    },
   },
 });
